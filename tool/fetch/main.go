304ec1bad075418dae0d87a4bd9ec6c1742c9552
o98k-ok <hggend@gmail.com>
2024-09-27 18:37:35 +0800

    [feature]<main>: support fetch run ✨✨

---
+++ tool/fetch/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+// parse https://icons8.com/icon/pGqqobAPSa_u/touchpad
+// to https://img.icons8.com/?size=250&id=pGqqobAPSa_u&format=png
+// to https://img.icons8.com/?size=250&id=pGqqobAPSa_u&format=png&color=FFFFFF
+func GenAllURLs(url string) map[string]string {
+	base := "https://img.icons8.com/?size=250&id="
+	fields := strings.Split(url, "/")
+	id := fields[len(fields)-2]
+
+	format := "&format=png"
+	color := "&color=FFFFFF"
+
+	urls := map[string]string{
+		"black": base + id + format,
+		"white": base + id + format + color,
+	}
+	return urls
+}
+
+func Download(urls map[string]string, filePrfix string) {
+	for color, url := range urls {
+		response, err := http.Get(url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer response.Body.Close()
+
+		body, err := io.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.WriteFile(fmt.Sprintf("%s_%s.png", filePrfix, color), body, 0o644)
+	}
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatal("Usage: go run main.go <url> <output_prefix>")
+	}
+
+	raw := os.Args[1]
+	urls := GenAllURLs(raw)
+	Download(urls, os.Args[2])
+}
